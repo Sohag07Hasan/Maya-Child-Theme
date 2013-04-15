843de56ceef146ff19b8d196561abf45aa11ed2f
Mahibul Hasan Sohag <hyde.sohag@gmail.com>
2013-04-16 03:52:58 +0600

    working

--- woocommerce/loop/rating.php
+++ woocommerce/loop/rating.php
@@ -2,6 +2,9 @@
 	global $post;
 	$rating =  get_post_meta( $post->ID, 'avg_rating', true );
 	$count = get_post_meta($post->ID, 'review_count', true);
+	
+	$count = ($count > 0) ? $count : 0;
+	$rating = ($rating > 0) ? $rating : 0;
 ?>
 
 <div class="loop_rating" itemtype="http://schema.org/AggregateRating" itemscope="" itemprop="aggregateRating">
@@ -10,6 +13,6 @@
 			<span class="rating" itemprop="ratingValue"><?php echo $rating; ?>  out of 5 </span>
 		</span>		
 	</div>
-	<span title="Total <?php echo $count; ?> Reviews" >( <?php echo $count ?> Reviews )</span>
+	<span title="Total <?php echo $count; ?> Reviews" >( <?php echo ($count) ? $count . ' Reviews' : 'No Reviews Yet' ?> )</span>
 </div>
 <div style="clear:both"></div>
